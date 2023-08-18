d0d600d51044f329dab9bbbe4598f27edde62363
DavidLucasK <davidlucasfr70@gmail.com>
2023-08-18 14:20:20 -0300

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,71 @@
+const navbar = document.querySelector('.navbar');
+const mobileNavbar = document.querySelector('.navbar__mobile');
+const button = document.querySelector('.burguer');
+
+// Animação com scroll
+
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 30) / 32);
+  target.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if(target.length) {
+  window.addEventListener('scroll', debounce(function() {
+    animeScroll();
+  }, 10));
+}
+
+//
+
+
+button.addEventListener('click', function () {
+  mobileNavbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function () {
+  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
+  return navbar.classList.remove('active');
+});
+
+/* Efeito de máquina de escrever */ 
+
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    console.log(i);
+      setTimeout(function() {
+        elemento.innerHTML += letra;
+      }, 75 * i)
+  });
+}
+
+const titulo = document.querySelector('h1');
+typeWriter(titulo);
